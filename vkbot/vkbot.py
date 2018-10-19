fdb8eda24f3a5e3f3b9f6b0d844a79104ea88e9a
shebunovanton <shebunov.an@gmail.com>
2018-10-19 18:10:19 +0300

    first commit

---
+++ vkbot/vkbot.py
@@ -0,0 +1,22 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+login, password = '89531235933', 'Zigzagsel1488'
+vk_session = vk_api.VkApi(login, password)
+vk_session.auth()
+vk = vk_session.get_api()
+
+#print(vk.wall.post(message='Hello world!'))
+longpoll = VkLongPoll(vk_session)
+
+
+for event in longpoll.listen():
+   # if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text :
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me :
+        print('id{}: "{}"'.format(event.peer_id, event.text), end=' ')
+
+        vk.messages.send(
+                    user_id=event.peer_id,
+                    message='No results'
+        )
